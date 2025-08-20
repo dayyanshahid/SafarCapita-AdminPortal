fcdabed470cd93a03d9245e2bb6d4bd116c2ee5b
unknown <hr3261331@gmail.com>
2025-08-20 17:47:40 +0500

    notification don

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Providers from "./providers";
 import "./globals.css";
-import FCM from "../components/FCM";
+import FCM from "../components/fcm";
 
 const inter = Inter({ subsets: ["latin"] });
 
